093f48ee5ab3c5131330a783e9b43ae8ea7294a2
13circle <13circle97@gmail.com>
2021-08-04 08:54:45 +0900

    ImageApp completed

--- ImageApp/frmImage_u.pas
+++ ImageApp/frmImage_u.pas
@@ -4,10 +4,13 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.StdCtrls,
+  Vcl.Imaging.pngimage;
 
 type
   TForm1 = class(TForm)
+    Image1: TImage;
+    Label1: TLabel;
   private
     { Private declarations }
   public
